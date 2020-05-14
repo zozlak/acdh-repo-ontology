2ae1caff01c1dabc8f6cb1116ed4f417c8f03ac9
Mateusz Żółtak <zozlak@zozlak.org>
2020-05-14 19:48:46 +0200

    CI workflow added

--- .github/workflows/test-postgresql_config.sh
+++ .github/workflows/test-postgresql_config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "listen_addresses = '*'" >> /home/www-data/postgresql/postgresql.conf
+sed -i -E 's/peer|ident|md5/trust/g' /home/www-data/postgresql/pg_hba.conf
 echo "host all all 127.0.0.1/0 trust" >> /home/www-data/postgresql/pg_hba.conf
+echo "host all all ::1/0 trust" >> /home/www-data/postgresql/pg_hba.conf
